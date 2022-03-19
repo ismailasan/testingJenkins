11b0610c741df395065bf31dfead06ea5e2ff1c8
ismailasan <ismailasan@gmail.com>
2022-03-19 14:06:36 -0400

    updated with test

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -1,10 +1,52 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class Test01 {
+import java.util.HashMap;
+import java.util.Map;
 
+import static io.restassured.RestAssured.given;
+
+public class Test01 {
     @Test
-    public void test(){
-        System.out.println("haydi bakalim");
+    public void apiTest(){
+        RequestSpecification spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+//set the base url and the path params
+        spec.pathParams("first", "booking", "second",3);
+
+//        Send th eGet request and get the response
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+//  {
+//    "firstname": "Mark",
+//    "lastname": "Brown",
+//    "totalprice": 640,
+//    "depositpaid": false,
+//    "bookingdates": {
+//        "checkin": "2017-08-07",
+//        "checkout": "2020-03-19"
+//    },
+//    "additionalneeds": "Breakfast"
+//}
+
+//        Setting actual data
+
+        Map<String, Object> actualData = response.as(HashMap.class);
+        System.out.println(actualData);
+
+        Map<String, Object> bookingDates = (Map)actualData.get("bookingdates");
+        System.out.println(bookingDates);
+
+        Assert.assertEquals("Mary", actualData.get("firstname"));
+        Assert.assertEquals("Jones", actualData.get("lastname"));
+        Assert.assertEquals(882, actualData.get("totalprice"));
+        Assert.assertEquals(true, actualData.get("depositpaid"));
+
+        Assert.assertEquals("2016-11-08", bookingDates.get("checkin"));
+        Assert.assertEquals("2019-05-02", bookingDates.get("checkout"));
 
     }
 
